512024ac1a2187581b851a70d9df9559decb7e92
march <francois.cousin62880@gmail.com>
2019-10-17 10:40:48 +0200

    les journées

--- public/index.php
+++ public/index.php
@@ -7,18 +7,42 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/calendar.css">
     <title>Calendrier</title>
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
-<a class="navbar-brand" href="/index.php">Mon calendrier</a>
-</nav>
-<?php 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
+        <a class="navbar-brand" href="/index.php">Mon calendrier</a>
+    </nav>
+    <?php 
 require '../src/Date/Month.php';
-$month = new App\Date\Month( 0, 2018); 
-?>
+try {
+$month = new App\Date\Month($_GET['month'] ?? null, $_GET['year'] ?? null); 
 
+}catch (\Exception $e){
+    $month = new App\Date\Month();
+}
+$start = $month->getStartingDay()->modify('Last monday');
+?>
+    <h1><?= $month->toString(); ?></h1>
+
+    <table class="calendar__table calendar__table--<?= $month->getWeeks(); ?>weeks">
+        <?php for($i = 0; $i<$month->getWeeks(); $i++): ?>
+        <tr>
+            <?php foreach($month->days as $k =>$day): ?>
+            <td>
+                <?php if($i === 0): ?>
+                <div class="calendar__weekday"> <?= $day; ?></div>
+                <?php endif; ?>
+                <div class="calendar__day"><?= (clone $start)->modify("+" . ($k + $i *7) . "days")->format('d'); ?>
+                </div>
+            </td>
+            <?php endforeach; ?>
+        </tr>
+
+        <?php endfor; ?>
+    </table>
 
 </body>
 
--- src/Date/Month.php
+++ src/Date/Month.php
@@ -4,6 +4,10 @@ namespace App\Date;
 
 class Month {
 
+    public $days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    private $months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+    private $month;
+    private $year;
     /**
      * Month contructor
      * @param int $month le mois compris entre 1 et 12
@@ -11,8 +15,16 @@ class Month {
      * @throws \Exception
      */
 
-    public function __construct(int $month, int $year)
+    public function __construct(?int $month=null, ?int $year=null)
     {
+        if($month === null)
+        {
+            $month= intval(date('m'));
+        }
+        if($year === null)
+        {
+            $year= intval(date('Y'));
+        }
         if ($month < 1 || $month > 12)
         {
             throw new \Exception("le mois $month n'est pas valide");
@@ -21,5 +33,36 @@ class Month {
         {
             throw new \Exception("l'année est inférieur à 1970");
         }
+        $this->month = $month;
+        $this->year = $year;
+    }
+    /**
+     * Renvoie le premier jour du mois
+     * @return \DateTime
+     */
+
+    public function getStartingDay(): \DateTime
+    {
+        return new \DateTime("{$this->year}-{$this->month}-01");
+    }
+
+    /**
+     * Retourne le mois en toute lettre (ex Mars 2018)
+     * @return string
+     */
+    public function toString(): string
+    {
+       return $this->months[$this->month - 1]. ' '. $this->year;
+    }
+    public function getWeeks(): int
+    {
+        $start = $this->getStartingDay();
+        $end = (clone $start)->modify('+1 month -1 day');
+        $weeks= intval($end->format('W'))-intval($start->format('W'))+1;
+        if($weeks < 0)
+        {
+            $weeks = intval($end->format('W'));
+        }
+        return $weeks;
     }
 }
